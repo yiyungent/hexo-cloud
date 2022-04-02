6ba2b863d8c7881557b354b60e5e85843115cb91
yiyun <yiyungent@gmail.com>
2022-04-03 00:57:30 +0800

    feat: extensions 通用扩展 配置方式

--- index.js
+++ index.js
@@ -9,10 +9,35 @@ hexo.extend.injector.register(
 
     if (!config.enable || !config.serverURL) return rtnStr;
 
+    //#region 扩展
+    let extensionsAddCookie = "";
+    if (config.hasOwnProperty("extensions") && config.extensions) {
+      for (const key in config.extensions) {
+        if (Object.hasOwnProperty.call(config.extensions, key)) {
+          const itemConfig = config.extensions[key];
+          let itemConfigJsonStr = JSON.stringify(item);
+          extensionsAddCookie  = extensionsAddCookie + `hexoCloudAddCookie("${key}", '${itemConfigJsonStr}', 24);`;
+        }
+      }
+    }
+    //#endregion 扩展
+
     let plugincoreVersion = config.plugincoreVersion;
     rtnStr += `<!-- PluginCore.IPlugins.IWidgetPlugin.Widget(hexo-cloud,${plugincoreVersion},body_end) -->
                <script src="${config.plugincoreLibURL}"></script>
                <script>
+                    function hexoCloudAddCookie(name, value, hours) {
+                      var str = "hexo_cloud." + name + "=" + escape(value);
+                      if (hours > 0) {
+                          var date = new Date();
+                          var ms = hours * 3600 * 1000;
+                          date.setTime(date.getTime() + ms);
+                          str += "; expires=" + date.toGMTString() + ";path=/";
+                      }
+                      document.cookie = str;
+                    }
+                    // 扩展
+                    ${extensionsAddCookie}
                     document.addEventListener('page:loaded', () => {
                         var hexoPluginCore = new PluginCore({
                                 baseUrl: "${config.serverURL}"
